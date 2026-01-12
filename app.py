aaaed313635f89ad763ff13bd86e4e1e15902996
PRATHAMESH SUHAS URAVANE <upratham2002@gmail.com>
2026-01-11 23:24:38 -0500

    app added to the project

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+#streamlit UI
+
+st.title("Power Calculator")
+st.write("Enter a number to calculate its square ,cube and fifth power")
+
+#Get user input 
+n= st.number_input("Enter an integer",value=1,step=1)
+
+#calculate
+square=n**2
+cube=n** 3
+fifth_power=n**5
+
+# display Results
+
+st.write(f'The square of {n} is :{square}')
+st.write(f'The cube of {n} is :{cube}')
+st.write(f'The fifth power of {n} is :{fifth_power}')
